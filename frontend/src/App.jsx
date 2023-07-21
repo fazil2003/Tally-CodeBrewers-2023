69f0f325b3366ad27ea9d724ddfacc0a97904641
Mohamed Fazil Z <mohamedfazil463@gmail.com>
2023-07-21 14:39:23 +0530

    [F] Added the Footer.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Practice from "./components/Practice";
 import "./App.css";
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Practice />}></Route>
         </Routes>
       </Router>
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p className="footer-name">Accuracy: <b>90%</b></p>
+            <p className="footer-name">Speed: <b>30 WPM</b></p>
+        </div>
+    )
+}
+
+export default Footer;
