261524a9bdc259fd162554994114555db018d825
muthuvijay5 <ymuthuvijay5@gmail.com>
2023-07-21 15:19:15 +0530

    [F] Fixed space problem

--- frontend/src/components/Practice.jsx
+++ frontend/src/components/Practice.jsx
@@ -83,6 +83,9 @@ function Practice() {
           typedWordsArray.length !== 1
         ) {
           typedWordsArray.shift();
+          while (typedWordsArray[0] === "") {
+            typedWordsArray.shift();
+          }
           ++i;
         }
 
@@ -165,53 +168,58 @@ function Practice() {
   }, []);
 
   const Ref = useRef(null);
- 
+
   // The state for our timer
-  const [timer, setTimer] = useState('00:00:00');
+  const [timer, setTimer] = useState("00:00:00");
 
   const getTimeRemaining = (e) => {
-	  const total = Date.parse(e) - Date.parse(new Date());
-	  const seconds = Math.floor((total / 1000) % 60);
-	  const minutes = Math.floor((total / 1000 / 60) % 60);
-	  const hours = Math.floor((total / 1000 / 60 / 60) % 24);
-	  return {
-		  total, hours, minutes, seconds
-	  };
-  }
+    const total = Date.parse(e) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+    return {
+      total,
+      hours,
+      minutes,
+      seconds,
+    };
+  };
 
   const startTimer = (e) => {
-	  let { total, hours, minutes, seconds } = getTimeRemaining(e);
-	  if (total >= 0) {
-		  setTimer(
-			  (hours > 9 ? hours : '0' + hours) + ':' +
-			  (minutes > 9 ? minutes : '0' + minutes) + ':'
-			  + (seconds > 9 ? seconds : '0' + seconds)
-		  )
-	  }
-  }
+    let { total, hours, minutes, seconds } = getTimeRemaining(e);
+    if (total >= 0) {
+      setTimer(
+        (hours > 9 ? hours : "0" + hours) +
+          ":" +
+          (minutes > 9 ? minutes : "0" + minutes) +
+          ":" +
+          (seconds > 9 ? seconds : "0" + seconds)
+      );
+    }
+  };
 
   const clearTimer = (e) => {
-	  setTimer('00:00:10');
-	  if (Ref.current) clearInterval(Ref.current);
-	  const id = setInterval(() => {
-		  startTimer(e);
-	  }, 1000)
-	  Ref.current = id;
-  }
+    setTimer("00:00:10");
+    if (Ref.current) clearInterval(Ref.current);
+    const id = setInterval(() => {
+      startTimer(e);
+    }, 1000);
+    Ref.current = id;
+  };
 
   const getDeadTime = () => {
-	  let deadline = new Date();
-	  deadline.setSeconds(deadline.getSeconds() + 10);
-	  return deadline;
-  }
+    let deadline = new Date();
+    deadline.setSeconds(deadline.getSeconds() + 10);
+    return deadline;
+  };
 
   useEffect(() => {
-	  clearTimer(getDeadTime());
+    clearTimer(getDeadTime());
   }, []);
 
   const onClickReset = () => {
-	  clearTimer(getDeadTime());
-  }
+    clearTimer(getDeadTime());
+  };
 
   //   useEffect(() => {
   //     function handleKeyDown(e) {
@@ -281,8 +289,7 @@ function Practice() {
           Set
         </button>
 
-		<p>{timer}</p>
-
+        <p>{timer}</p>
       </div>
 
       <p className="sentence">{textSpans}</p>
