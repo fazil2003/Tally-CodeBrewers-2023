34a778f6b92f5ec8c6721a53d41ffbde7794be99
Mohamed Fazil Z <mohamedfazil463@gmail.com>
2023-07-22 10:57:35 +0530

    Added the functionalities in the Home Component.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,8 +4,28 @@ import defaultVariables from "../variables";
 import Footer from "./footer";
 
 function Home() {
+
+    function goPractice(){
+        window.location = defaultVariables.frontendUrl + "/practice";
+    }
+
+    function goPrivate(){
+        window.location = defaultVariables.frontendUrl + "/private";
+    }
+
   return (
-    <></>
+    <div className="container">
+        <center>
+            <div className="home-menu">
+                <div className="component" onClick={goPractice}>
+                    Practice
+                </div>
+                <div className="component" onClick={goPrivate}>
+                    Private
+                </div>
+            </div>
+        </center>
+    </div>
   );
 }
 
